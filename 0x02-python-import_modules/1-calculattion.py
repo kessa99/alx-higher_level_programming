3465a079110c38749b40c19633c7371790273b8a
kessa99 <kessadavidkipre@gmail.com>
2023-03-14 12:26:51 +0000

    the pus

--- 0x02-python-import_modules/1-calculattion.py
+++ 0x02-python-import_modules/1-calculattion.py
@@ -9,4 +9,15 @@ if __name__ == "__main__":
     print("{} + {} = {}".format(a, b, result1))
     print("{} - {} = {}".format(a, b, result2))
     print("{} * {} = {}".format(a, b, result3))
+    print("{} \ {} = {}".format(a, b, result4))if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    result1 = add(a, b)
+    result2 = sub(a, b)
+    result3 = mul(a, b)
+    result4 = div(a, b)
+    print("{} + {} = {}".format(a, b, result1))
+    print("{} - {} = {}".format(a, b, result2))
+    print("{} * {} = {}".format(a, b, result3))
     print("{} \ {} = {}".format(a, b, result4))
