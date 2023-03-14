1ec8b79e8e759d6d874db26da7679bbb0540130a
kessa99 <kessadavidkipre@gmail.com>
2023-03-14 11:43:34 +0000

    the calculator

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+    from calculator_1 import add
+    from calculator_1 import sub
+    from calculator_1 import mul
+    from calculator_1 import div
+    a = 10
+    b = 5
+    result1 = add(a, b)
+    result2 = sub(a, b)
+    result3 = mul(a, b)
+    result4 = div(a, b)
+    print("{} + {} = {}".format(a, b, result1))
+    print("{} + {} = {}".format(a, b, result2))
+    print("{} + {} = {}".format(a, b, result3))
+    print("{} + {} = {}".format(a, b, result4))
