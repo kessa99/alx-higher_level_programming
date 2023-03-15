875baf85b085c757af97e2661760c6019b0a8ce9
kessa99 <kessadavidkipre@gmail.com>
2023-03-15 02:25:47 +0000

    Dynamic script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 import sys
 
-print("{} arguments".format(len(sys.argv) - 1))
+if len(sys.argv) == 1:
+    print("0 argument.")
+elif len(sys.argv) > 1:
+    print("{} arguments".format(len(sys.argv) - 1))
+
 for i in range(1, len(sys.argv)):
     print("{}: {}".format(i, sys.argv[i]))
