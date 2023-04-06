e81b0dc85eca9bd8b884c992522b0d6810ca805b
kessa99 <kessadavidkipre@gmail.com>
2023-04-06 01:14:17 +0000

    the commit for the merge work

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif len(sys.argv) == 2:
         print("{} argument:".format(len(sys.argv) - 1))
-    elif len (sys.argv) > 2:
+    elif len(sys.argv) > 2:
         print("{} arguments:".format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
