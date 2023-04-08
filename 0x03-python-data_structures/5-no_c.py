dba416114ef24457fed72cf7274cc4a988a00efc
kessa99 <kessadavidkipre@gmail.com>
2023-04-08 15:22:38 +0000

    Remove all kind of c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
     chaine = list(my_string)
-    for char in chaine:
-        if char in ['c', 'C']:
-            chaine.remove(char)
+    idx = 0
+    while idx < len(chaine):
+        if chaine[idx] in ['c', 'C']:
+            chaine.pop(idx)
+        else:
+            idx += 1
     new_string = ''.join(chaine)
     return new_string
