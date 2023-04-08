1ed96747a903c04d276ba25d315e1a14849bed7c
kessa99 <kessadavidkipre@gmail.com>
2023-04-08 11:12:34 +0000

    Build my own calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from calculator_1 import add, mul, sub, div
     a = 10
-    b = 5
+    b = 1
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
