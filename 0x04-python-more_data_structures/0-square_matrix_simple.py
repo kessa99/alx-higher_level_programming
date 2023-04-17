c3ec9b223937c4cc52599d02e7fa785ca6115a9d
kessa99 <kessadavidkipre@gmail.com>
2023-04-17 12:57:44 +0000

    Simple squard

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,9 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new_matrix = []
-    for row in matrix:
+    new_matrix1 = matrix.copy()
+    new_matrix2 = []
+    for row in new_matrix1:
         fonction_sqrt = lambda x: x ** 2
         new_row = list(map(fonction_sqrt, row))
-        new_matrix.append(new_row)
-    return new_matrix
+        new_matrix2.append(new_row)
+    return new_matrix2
