7a6ff1581d065226b73808c1192ead424aae759f
kessa99 <kessadavidkipre@gmail.com>
2023-04-18 09:06:10 +0000

    Printed sorted dictionary udapt with sorted

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for value, keys in a_dictionary.items():
+    for value, keys in sorted(a_dictionary.items()):
         print("{}: {}".format(value, keys))
