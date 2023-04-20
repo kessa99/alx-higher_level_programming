af6c009ecc909fa1566f5a11ace129dc2f7b269d
kessa99 <kessadavidkipre@gmail.com>
2023-04-20 03:34:57 +0000

    My frst exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for item in my_list:
-        try:
-            if count < x:
-                print(item, end=" ")
-                count += 1
-            else:
-                break
-        except:
-            break
+    try :
+        for idx in range(x):
+            print("{}".format(my_list[idx]), end ="")
+            count += 1
+    except IndexError:
+            pass
     print()
+    return count
