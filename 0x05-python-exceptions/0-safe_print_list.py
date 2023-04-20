48583e2a88ca5bca8ad94709837cef56a720c3d3
kessa99 <kessadavidkipre@gmail.com>
2023-04-20 07:24:49 -0700

    Update 6-raise_excption

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    try :
+    try:
         for idx in range(x):
-            print("{}".format(my_list[idx]), end ="")
+            print("{}".format(my_list[idx]), end="")
             count += 1
     except IndexError:
         pass
