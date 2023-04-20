4f9bdf07348f408b802be88524b56ef004404418
kessa99 <kessadavidkipre@gmail.com>
2023-04-20 04:21:30 +0000

    Udapt for the excercice of exception

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         for idx in range(x):
             if isinstance(my_list[idx], int):
-                print("{}".format(my_list[idx]), end="")
+                print("{:d}".format(my_list[idx]), end="")
                 count += 1
             else:
                 pass
