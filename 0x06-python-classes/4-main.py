032295d6fa0789571bbea59b878e5496987c2fa8
kessa99 <kessadavidkipre@gmail.com>
2023-04-26 10:07:09 -0700

    Use of getter and setter

--- 0x06-python-classes/4-main.py
+++ 0x06-python-classes/4-main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-Square = __import__('4-square').Square
-my_square = Square()
-print(mysquare.size)
+if __name__ == "__main__":
+    Square = __import__('4-square').Square
+    my_square = Square()
+    print(mysquare.size)
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,13 +4,11 @@
 
 class Square:
     """Calcul and modification"""
-    def __init__(self, size):
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        elif not size:
-            self.__size = 0
         else:
             self.__size = size
 
