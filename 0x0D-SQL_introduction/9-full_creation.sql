ac34544266eda8cd14ecf2ae018b6202e80fbf47
kessa99 <kessadavidkipre@gmail.com>
2023-05-17 13:59:11 -0700

    Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
 -- creat a table second_table in the database.
 CREATE TABLE IF NOT EXISTS second_table(
-	id INT,
-	name VARCHAR(256),
-	score INT
+	`id` INT,
+	`name` VARCHAR(256),
+	`score` INT
 );
 
 -- add lign to second_table.
