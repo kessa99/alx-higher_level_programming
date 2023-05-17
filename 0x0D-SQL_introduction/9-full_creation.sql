7cfd5ded66f17ebb6e89b2ee351fbb48f0f00c14
kessa99 <kessadavidkipre@gmail.com>
2023-05-17 13:57:06 -0700

    Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,4 +6,4 @@ CREATE TABLE IF NOT EXISTS second_table(
 );
 
 -- add lign to second_table.
-INSERT INTO second_table (id, name, score) VALUES(1, "John", 10), VALUES(2, "Alex", 3), VALUES(3, "Bob", 14), VALUES(4, "George", 18);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES(1, "John", 10), VALUES(2, "Alex", 3), VALUES(3, "Bob", 14), VALUES(4, "George", 18);
