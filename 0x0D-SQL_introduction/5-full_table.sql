c7d8d7ca271a1ee0c2f53817d7b6bb4b42b59ceb
kessa99 <kessadavidkipre@gmail.com>
2023-05-17 13:55:04 -0700

    Full creation

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,4 @@
 --prints the full descriptions of the first_table.
-SHOW CREATE TABLE `first_table`;
+SELECT column_name, column_type, is_nullable, column_key, column_default, extra
+FROM information_schema.columns
+WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,7 +4,6 @@ CREATE TABLE IF NOT EXISTS second_table(
 	name VARCHAR(256),
 	score INT
 );
-INSERT INTO second_table (id, name, score) VALUES(1, "John", 10);
-INSERT INTO second_table (id, name, score) VALUES(2, "Alex", 3);
-INSERT INTO second_table (id, name, score) VALUES(3, "Bob", 14);
-INSERT INTO second_table (id, name, score) VALUES(4, "George", 18);
+
+-- add lign to second_table.
+INSERT INTO second_table (id, name, score) VALUES(1, "John", 10), VALUES(2, "Alex", 3), VALUES(3, "Bob", 14), VALUES(4, "George", 18);
