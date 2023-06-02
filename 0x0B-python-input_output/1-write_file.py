765be2596fe99785e1c0a8567b59295e1a82010d
kessa99 <kessadavidkipre@gmail.com>
2023-06-02 10:44:45 -0700

    write file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Prototype: def write_file(filename="", text=""):
+write_file(filename="", text="") -
 You must use the with statement
 You don’t need to manage file permission exceptions.
 Your function should create the file if doesn’t exist.
@@ -8,5 +8,8 @@ Your function should overwrite the content of the file if it already exists.
 You are not allowed to import any module
 """
 def write_file(my_first_file="", text=""):
+    """
+    write in file
+    """
     with open('my_first_file', 'w', encoding="utf-8") as f:
         return f.write(text)
