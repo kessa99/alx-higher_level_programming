3c766523e01165e4e179af37f0896443f9c77f71
kessa99 <kessadavidkipre@gmail.com>
2023-06-05 06:09:41 -0700

    return JSON representation

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/pythoni3
+import json
+"""
+Prototype: def to_json_string(my_obj)
+You don’t need to manage exceptions
+if the object can’t be serialized.
+"""
+
+
+def to_json_string(my_dict):
+    """
+    returns json representation
+    """
+    return json.dumps(my_dict)
