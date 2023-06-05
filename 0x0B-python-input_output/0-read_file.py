0091ea8612647bee35343ab0f073312fb960989d
kessa99 <kessadavidkipre@gmail.com>
2023-06-05 02:10:27 -0700

    rea a file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Prototype: def read_file(filename=""):
+    You must use the with statement
+    You don’t need to manage file permission or file doesn't exist exceptions.
+    You are not allowed to import any module
+"""
+def read_file(my_file_0=""):
+    """
+    Read a file
+    """
+    with open('my_file_0.txt', 'r', encoding="utf-8") as files:
+        show = files.read()
+        print(show)
