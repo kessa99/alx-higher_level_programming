2cacf59f4be8e5614387d47c7c418be90232ca3a
kessa99 <kessadavidkipre@gmail.com>
2023-06-05 08:55:13 -0700

    open file and do json with dump

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -17,4 +17,4 @@ def save_to_json_file(my_obj, filename):
     save in json
     """
     with open(filename, 'w', encoding="utf-8") as f:
-        json.dumps(my_obj, f)
+        json.dump(my_obj, f)
