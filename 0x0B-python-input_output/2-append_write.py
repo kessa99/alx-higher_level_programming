464b46538e8645900af80648d52f004cf00320a9
kessa99 <kessadavidkipre@gmail.com>
2023-06-05 03:37:57 -0700

    append in file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Prototype: def append_write(filename="", text=""):
+    If the file doesn’t exist, it should be created
+    You must use the with statement
+    You don’t need to manage file permission
+    or file doesn't exist exceptions.
+    You are not allowed to import any module
+"""
+
+
+def append_write(filename="", text=""):
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
