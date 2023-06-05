42604ebea42ab56136251aac6691613698d8296f
kessa99 <kessadavidkipre@gmail.com>
2023-06-05 10:40:14 -0700

    write the script for load and save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -18,12 +18,13 @@ import os.path
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-my_list = []
 
 if os.path.exists("add_item.json"):
     list_of_json = load_from_json_file("add_item.json")
+else:
+    list_of_json = []
 
 for arg in sys.argv[1:]:
-    my_list.append(arg)
+    list_of_json.append(arg)
 
-save_to_json_file(my_list, 'add_item.json')
+save_to_json_file(list_of_json, 'add_item.json')
