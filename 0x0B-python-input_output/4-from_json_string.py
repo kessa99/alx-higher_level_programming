0f8d59c831214c22f07b62f11bd58586dbb04b88
kessa99 <kessadavidkipre@gmail.com>
2023-06-05 06:36:15 -0700

    Retruns Python object from JSON string

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """
     Prototype: def from_json_string(my_str):
-    You don’t need to manage exceptions 
+    You don’t need to manage exceptions
     if the JSON string doesn’t represent an object.
 """
 import json
-
 
 
 def from_json_string(my_str):
