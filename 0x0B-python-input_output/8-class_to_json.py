b3a74adaa5cbed8a6e106c1c8a55171b3b26ede2
kessa99 <kessadavidkipre@gmail.com>
2023-06-05 19:08:47 -0700

    decription of dictionary

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    Prototype: def class_to_json(obj):
+    obj is an instance of a Class
+    All attributes of the obj Class are serializable:
+    list, dictionary, string, integer and boolean
+    You are not allowed to import any module
+"""
+import json
+
+
+def class_to_json(obj):
+    """
+        creat a empty dictionnary to save the description
+        Obtain the value of attribute
+        Check value type and move on the empty dict
+    """
+    json_dict = {}
+
+    for attr in obj.__dict__:
+        value = getattr(obj, attr)
+
+        if isinstance(value, (list, dict, str, int, bool)):
+            json_dict[attr] = value
+    return json_dict
