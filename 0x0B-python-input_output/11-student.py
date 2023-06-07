468d66c42c9f9cc92d1bbcedf12e3d02b9dd533e
kessa99 <kessadavidkipre@gmail.com>
2023-06-07 02:50:51 -0700

    Update the 9-student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+"""
+class Student:
+    """
+    """
+
+
+    def __init__(self, first_name, last_name, age):
+        """
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        """
+
+        if attrs is None:
+            attrs = self.__dict__keys()
+
+        value = None
+        json_dict = {}
+
+        for attr in attrs:
+            if attr in self.__dict__:
+                value = getattr(self. attr)
+
+                if isinstance(value, (list, str, int, dict, bool)):
+                    json_dict[attr] = value
+        return json_dict
+
+    def reload_from_json(self, json):
+        """
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
