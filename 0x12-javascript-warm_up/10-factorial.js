65c2f36a8f760d2bf006483a890088f2bf098da3
kessa99 <kessadavidkipre@gmail.com>
2023-06-13 13:26:03 -0700

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+function fact (n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * fact(n - 1);
+  }
+}
+const arg = parseInt(process.argv[2]);
+const num = isNaN(arg);
+if (num) {
+  console.log('NaN');
+} else {
+  const result = fact(arg);
+  console.log(result);
+}
