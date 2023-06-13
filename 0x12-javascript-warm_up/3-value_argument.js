187df71d2855d1ff6f7d6fdb6b01224fdc8ce067
kessa99 <kessadavidkipre@gmail.com>
2023-06-13 02:23:16 -0700

    update print of argument element

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,8 +3,6 @@
 if (!process.argv[2]) {
   console.log('No argument');
 }
-for (let i = 2; i < process.argv.length; i++) {
-  if (process.argv[i] !== 2) {
-    console.log(process.argv[i]);
-  }
+else{
+  console.log(process.argv[2])
 }
