dea5c0913f53f0472fc7373a66f396e4ffcbd329
kessa99 <kessadavidkipre@gmail.com>
2023-06-13 07:42:27 -0700

    Script to print a integer

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -6,7 +6,7 @@ const str2 = 'undefined';
 if (!process.argv[2]) {
   console.log('undefined is undefined');
 } else if (process.argv.length === 3) {
-  const result = process.argv[2].concat(' ', str1, ' ',  str2);
+  const result = process.argv[2].concat(' ', str1, ' ', str2);
   console.log(result);
 } else if (process.argv.length >= 3) {
   const result = process.argv[2] + ' ' + str1 + ' ' + process.argv[3];
