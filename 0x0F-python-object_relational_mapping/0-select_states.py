54d93d39c5ebd842c9758b864f8427ef121d1b3f
kessa99 <kessadavidkipre@gmail.com>
2023-06-19 04:00:17 -0700

    Update data server

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,14 +8,13 @@ import subprocess
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost", port = 3306, user = sys.argv[1],
-        password = sys.argv[2], db = sys.argv[3]
-        )
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+        password=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     query = "SELECT *FROM states ORDER BY states.id ASC"
     cursor.execute(query)
 
-    result = cursor.ferchall()
+    result = cursor.fetchall()
     for row in result:
         print(row)
 
