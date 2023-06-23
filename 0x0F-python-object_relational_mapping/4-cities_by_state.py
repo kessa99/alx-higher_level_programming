1b603a5cc670e4521f66ce3bee3bc5fda824d919
kessa99 <kessadavidkipre@gmail.com>
2023-06-23 05:36:31 -0700

    udapt script python sql

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,24 +1,3 @@
 #!/usr/bin/python3
-"""Cities by states"""
+""" Cities by states"""
 
-import MySQLdb
-import sys
-
-if __name__ == 'main':
-    usr = sys.argv[1]
-    pwd = sys.argv[2]
-    db_nm = sys.argv[3]
-
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=usr, password=pwd, db=db_nm)
-    cursor = db.cursor()
-    query = "SELECT cities.id, cities.name, states.name FROM cities, states WHERE states.id=cities.state_id ORDER BY id ASC;"
-    cursor.execute(query)
-
-    result = db.fetchall()
-
-    for row in result:
-        print(row)
-
-    cursor.close()
-    db.close()
