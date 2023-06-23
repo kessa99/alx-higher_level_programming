da867432b37595f40e15ca777ec6d93884277d07
kessa99 <kessadavidkipre@gmail.com>
2023-06-23 05:49:53 -0700

    udapt sql

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,11 +7,11 @@ import MySQLdb
 import sys
 
 
-usr = sys.argv[1]
-pwd = sys.argv[2]
-db_nm = sys.argv[3]
-
 if __name__ == '__main__':
+    usr = sys.argv[1]
+    pwd = sys.argv[2]
+    db_nm = sys.argv[3]
+
     db = MySQLdb.connect(host='localhost', port=3306, user=usr,
                          password=pwd, db=db_nm)
     cursor = db.cursor()
