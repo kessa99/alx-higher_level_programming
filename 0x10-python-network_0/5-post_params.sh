9dfa3bcdbb5433129fa0cb76f0e9aec7b94ed0f1
kessa99 <kessadavidkipre@gmail.com>
2023-07-04 19:13:39 -0700

    cURL paramet

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #crist to post
-curl -sXPOST "$1" -d "email:test@gmail.com&subject:I will always be here for PLD"
+curl -sX POST -d "email:test@gmail.com&subject:I will always be here for PLD" "$1"
