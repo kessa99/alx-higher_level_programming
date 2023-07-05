d992c9648daf2876fbdef67914504590a23c6dae
kessa99 <kessadavidkipre@gmail.com>
2023-07-05 15:31:11 -0700

    Response header value #0

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -12,10 +12,12 @@ You must use a with statement
 
 import sys
 import urllib.request
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    head = response.getheaders()
 
-for response in head:
-    if response[0] == 'X-Request-Id':
-        print(response[1])
+if __name__ == '__main__':
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        head = response.getheaders()
+
+    for response in head:
+        if response[0] == 'X-Request-Id':
+            print(response[1])
