294f7cb2019480ffcc24a29c91250d67256cb322
kessa99 <kessadavidkipre@gmail.com>
2023-07-05 15:17:55 -0700

    Response header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+"""
+You must use the packages urllib and sys
+You are not allow to import
+packages other than urllib and sys
+The value of this variable is
+different for each request
+You don’t need to check arguments
+passed to the script (number or type)
+You must use a with statement
+"""
+
 import sys
 import urllib.request
 url = sys.argv[1]
