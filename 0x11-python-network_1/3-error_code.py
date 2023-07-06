0d9159524a69b33aa86c7d898dba44a422be5e71
kessa99 <kessadavidkipre@gmail.com>
2023-07-05 18:56:58 -0700

    Python script for Error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -22,4 +22,4 @@ if __name__ == '__main__':
             body = response.read().decode('utf-8')
             print(body)
     except urllib.error.HTTPError as e:
-        print("Error code:{}".f(e.code))
+        print("Error code:{}".format(e.code))
