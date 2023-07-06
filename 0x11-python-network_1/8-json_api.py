6433392898dbd5b05a07509a2aa193ad4c73ba67
kessa99 <kessadavidkipre@gmail.com>
2023-07-06 05:15:41 -0700

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+The letter must be sent in the variable q
+If no argument is given, set q=""
+If the response body is properly JSON
+formatted and not empty, display the
+id and name like this: [<id>] <name>
+Otherwise:
+Display Not a valid JSON if the JSON is invalid
+Display No result if the JSON is empty
+You must use the package requests and sys
+You are not allowed to import packages
+other than requests and sys
+"""
+if __name__ == '__main__':
+    import requests
+    import sys
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv) == 1:
+        q = ''
+    else:
+        q = sys.argv[1]
+
+    r = requests.post(url, data={'q': q})
+    try:
+        sortie = r.json()
+        if sortie:
+            print("[{}] {}".format(sortie['id'], sortie['name']))
+        else:
+            print("No result")
+    except json.JSONDecodeError:
+        print("Not a valid JSON")
