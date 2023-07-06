04bc9918996be293071be7a924ce186851291e90
kessa99 <kessadavidkipre@gmail.com>
2023-07-06 00:39:19 -0700

    Post n email#1

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 """
+The email must be sent in the variable email
+You must use the packages
+requests and sys
+You are not allowed to import packages
+other than requests and sys
+You don’t need to error check arguments
+passed to the script (number or type)
 """
 
 import requests
