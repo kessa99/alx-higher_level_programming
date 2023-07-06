978dce64e9a1e353e476d58f921ee5a225d0818e
kessa99 <kessadavidkipre@gmail.com>
2023-07-06 07:42:46 -0700

    Search ap

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+The letter must be sent in the variable q
+If no argument is given, set q=""
+If the response body is properly JSON
+formatted and not empty, display
+the id and name like this: [<id>] <name>
+Otherwise:
+Display Not a valid JSON if the JSON is invalid
+Display No result if the JSON is empty
+You must use the package requests and sys
+You are not allowed to import
+packages other than requests and sys
+"""
+
+
+if __name__ == '__main__':
+    import requests
+    import sys
+    import json
+
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    url = 'https://api.github.com/users/user'
+
+    r = requests.get(url, auth=(user, pwd))
+    if r.status_code == 200:
+        user_dt = r.json()
+        user_id = user_dt['id']
+        print(user_id)
+    else:
+        print("Error:", r.status_code)
