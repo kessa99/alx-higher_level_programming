32399d65c88377c6b3962c9a0b17a106f6859409
kessa99 <kessadavidkipre@gmail.com>
2023-07-19 03:46:08 -0700

    update recursivity in javascript

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,7 +2,7 @@
 
 function fact (n) {
   if (n === 0 || n === 1) {
-    return 1;
+    return n;
   } else {
     return n * fact(n - 1);
   }
@@ -11,7 +11,7 @@ function fact (n) {
 const arg = parseInt(process.argv[2]);
 const num = isNaN(arg);
 if (num) {
-  console.log('NaN');
+  console.log('1');
 } else {
   const result = fact(arg);
   console.log(result);
