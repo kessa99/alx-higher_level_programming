6ab7f496a5e2ab629369bbfb0bc834bfde6ee79e
kessa99 <kessadavidkipre@gmail.com>
2023-07-20 05:21:14 -0700

    creat empty object

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,8 +3,8 @@
 module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0 && typeof (h) === 'number' && typeof (w) === 'number') {
-      this.w = w;
-      this.h = h;
+      this.width = w;
+      this.height = h;
     }
   }
 };
