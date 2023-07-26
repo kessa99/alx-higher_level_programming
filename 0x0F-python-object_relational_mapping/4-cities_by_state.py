e79d7a2d74c2d70c726a7f547e6865b7dcdf3019
kessa99 <kessadavidkipre@gmail.com>
2023-07-26 03:21:39 -0700

    Updat documentations

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 """
-    Your script should take 3 arguments: mysql username, mysql p    assword and database name
+    Your script should take 3 arguments: mysql username
     You must use the module MySQLdb (import MySQLdb)
-    Your script should connect to a MySQL server running on localhost at port 3306
-    Results must be sorted in ascending order by cities.id
-    You can use only execute() once
-    Results must be displayed as they are in the example below
-    Your code should not be executed when imported
+    Your script should connect to a MySQL server running on loc
 """
 
 import MySQLdb
