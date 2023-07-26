4bff8b9416e26fa4e57f9492700b81be38014adc
kessa99 <kessadavidkipre@gmail.com>
2023-07-26 03:09:03 -0700

    Udapt Cities to states

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """
-    Cities by states.
+    Your script should take 3 arguments: mysql username, mysql p    assword and database name
+    You must use the module MySQLdb (import MySQLdb)
+    Your script should connect to a MySQL server running on localhost at port 3306
+    Results must be sorted in ascending order by cities.id
+    You can use only execute() once
+    Results must be displayed as they are in the example below
+    Your code should not be executed when imported
 """
 
 import MySQLdb
