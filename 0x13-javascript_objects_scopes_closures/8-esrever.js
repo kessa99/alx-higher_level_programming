db027e6be37775488cc64f1d289defd05a61be98
kessa99 <kessadavidkipre@gmail.com>
2023-08-16 15:05:29 -0700

    updat 3 tak

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  let newlist = [];
-  for (id = list.length - 1; id >= 0; id--) {
+  const newlist = [];
+  for (let id = list.length - 1; id >= 0; id--) {
     newlist.push(list[id]);
   }
   return newlist;
