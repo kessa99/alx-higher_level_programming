217d3939b292c15a2d8cf5171d7f91bd41c8673f
kessa99 <kessadavidkipre@gmail.com>
2023-08-16 10:12:54 -0700

    Log me

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-	let count = 0;
-	for (let id = 0; id < list.length; id++) {
-		if (list[id] === searchElement) {
-			count++;
-		}
-	}
-	return count;
+  let count = 0;
+    for (let id = 0; id < list.length; id++) {
+      if (list[id] === searchElement) {
+        count++;
+      }
+    }
+  return count;
 };
